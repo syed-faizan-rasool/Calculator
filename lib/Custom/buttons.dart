7c2ff7ebb029e1c259b4749f485b11b4472e1578
syed-faizan-rasool <163971167+syed-faizan-rasool@users.noreply.github.com>
2024-08-09 18:04:07 +0500

    ok

--- lib/Custom/buttons.dart
+++ lib/Custom/buttons.dart
@@ -1,27 +1,37 @@
-
 import 'package:flutter/material.dart';
-import 'package:calculator/View/home.dart';
-
 
 class CustomButtons extends StatelessWidget {
   final Color? colors;
   final String title;
-  
   final VoidCallback onpressed;
-  const CustomButtons({super.key,this.colors , required this.title , required this.onpressed});
+
+  const CustomButtons({
+    super.key,
+    this.colors,
+    required this.title,
+    required this.onpressed,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(7),
-      child: Container(
-        // padding: const EdgeInsets.symmetric(horizontal: 0),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50), color: colors ?? Colors.grey),
-        // color: graycolor,
-        width: 65,
-        height: 65,
-        child: Center(child: Text(title)),
+      child: GestureDetector(
+        onTap: onpressed, 
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: colors ?? Colors.grey,
+          ),
+          width: 65,
+          height: 65,
+          child: Center(
+            child: Text(
+              title,
+              style: const TextStyle(color: Colors.white), // Styling the text
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/View/home.dart
+++ lib/View/home.dart
@@ -1,10 +1,9 @@
 import 'package:calculator/Custom/buttons.dart';
 import 'package:calculator/Custom/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class CustomCalculator extends StatefulWidget {
-  
   const CustomCalculator({super.key});
 
   @override
@@ -12,327 +11,188 @@ class CustomCalculator extends StatefulWidget {
 }
 
 class _CustomCalculatorState extends State<CustomCalculator> {
+  var input = '';
+  var output = '';
+  var num1 = 0.0;
+  var num2 = 0.0;
+  var operations = "";
+
+  void onBtnClick(String value) {
+
+if (value == "AC") {
+    input = '';
+   output = '';
+}else if(value == "<<"){
+  if(input.isNotEmpty){
+  input = input.substring(0,input.length - 1);
+  }
+}else if(value == "="){
+  if (input.isNotEmpty){
+  var userInput = input;
+  userInput = input.replaceAll("x", "*");
+  Parser p = Parser();
+  Expression expression = p.parse(userInput);
+  ContextModel cm = ContextModel();
+  var finalValue = expression.evaluate(EvaluationType.REAL, cm);
+  output = finalValue.toString();
+ 
+
+ print("Output of $input is :  $output");
+}
+}
+else{
+  input = input + value;
+}
+
+
+    setState(() {});
+
+  }
+
   
-   
-   var Input = '';
-   var Output  = '';
-   
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.blackcolor,
-     
-        body:Expanded(
-          child: Column(
-            children: [
-              // const SizedBox(height: 130,),
-              Container(width: 500,
-                color: AppColors.graycolor,
-                child:  Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            SizedBox(height: 30,),
-            
-             Row(mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                 
-                     PopupMenuButton(
-                icon: const Icon(
-                  Icons.more_vert_outlined,
-                  color: Colors.white,
-                ),
-                itemBuilder: (context) => [
-                      const PopupMenuItem(
-                          child: Text(
-                        'Settings',
-                        style: TextStyle(color: Color.fromARGB(255, 5, 5, 5)),
-                      )),
-                    ]),
-                   
-                  SizedBox(
-                    width: 10,
-                  )
-                ],
-              )
-          ],
-                )),
-          
-             Expanded(
-               child: Container(
-                decoration: const BoxDecoration(
-                color: AppColors.graycolor,
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30),bottomRight: Radius.circular(30),),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                width: double.infinity,
-                 child:  Column(
-                  
+      body: Column(
+        children: [
+          // const SizedBox(height: 130,),
+          Container(
+            width: 500,
+            color: AppColors.graycolor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const SizedBox(height: 30,),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                  
-                    Text(Input.toString(),style: const TextStyle(fontSize: 30 , color: AppColors.whitecolor)),
-                   
-              const SizedBox(height: 20,),
-                    // Text(Answer.toString(),style: TextStyle(fontSize: 30 , color: AppColors.whitecolor)),
-                    Text(Output.toString(),style: TextStyle(fontSize: 20 , color: AppColors.whitecolor.withOpacity(0.7))),
-          
-                 
+                    PopupMenuButton(
+                      icon: const Icon(
+                        Icons.more_vert_outlined,
+                        color: Colors.white,
+                      ),
+                      itemBuilder: (context) => [
+                        const PopupMenuItem(
+                          child: Text(
+                            'Settings',
+                            style: TextStyle(color: Color.fromARGB(255, 5, 5, 5)),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    )
                   ],
-                 ),
-               ),
-             ),
-             const SizedBox(height: 20,),
-               Row(
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: AppColors.graycolor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              width: double.infinity,
+              child:  Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  const SizedBox(width: 13,),
-                 CustomButtons(colors: AppColors.browncolor, title: '2nd', onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.browncolor, title: 'rad', onpressed: () {
-                   Input = 'rad';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                  CustomButtons(colors: AppColors.browncolor, title: 'sin', onpressed: () {
-                   Input = 'sin';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.browncolor, title: 'cos',onpressed: () {
-                   Input = 'cos';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.browncolor, title: 'tan',onpressed: () {
-                   Input = 'tan';
-                   setState(() {
-                     ();
-                   });
-                 },),
+                  Text(input, style: TextStyle(fontSize: 35, color: AppColors.whitecolor,fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 20,),
+                  Text(output, style: TextStyle(fontSize: 25, color: AppColors.whitecolor.withOpacity(0.7))),
+                  const SizedBox(height: 10,),
+
                 ],
               ),
-               Row(
-                children: [
-                  const SizedBox(width: 13,),
-                 CustomButtons(colors: AppColors.browncolor, title: 'x^y',onpressed: () {
-                   Input = 'x^y';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.browncolor, title: 'lg',onpressed: () {
-                   Input = 'lg';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.browncolor, title: 'ln',onpressed: () {
-                   Input = 'ln';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.browncolor, title: '(', onpressed: () {
-                   Input = '(';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.browncolor, title: ')',onpressed: () {
-                   Input = ')';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                ],
-              ),
-               Row(
-                children: [
-                  const SizedBox(width: 13,),
-                 CustomButtons(colors: AppColors.browncolor, title: 'sqrt(x)',onpressed: () {
-                   Input = 'sqrt(x)';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.ambercolor, title: 'AC',onpressed: () {
-                   Input = 'AC';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.ambercolor, title: '<<',onpressed: () {
-                   Input = '<<';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.ambercolor, title: '%',onpressed: () {
-                   Input = '%';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.ambercolor, title: '/',onpressed: () {
-                   Input = '/';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                ],
-              ),
-              Row(
-                children: [
-                  const SizedBox(width: 13,),
-                 CustomButtons(colors: AppColors.browncolor, title: 'x!',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor , title: '7',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '8',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '9',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.ambercolor, title: 'X',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                ],
-              ),
-               Row(
-                children: [
-                  const SizedBox(width: 13,),
-                 CustomButtons(colors:AppColors.browncolor, title: '1/x',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '4',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '5',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '6',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.ambercolor, title: '-',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                ],
-              ),
-               Row(
-                children: [
-                  const SizedBox(width: 13,),
-                 CustomButtons(colors:AppColors.browncolor, title: 'pi',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '1',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '2',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '3',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.ambercolor, title: '+',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                ],
-              ),
-               Row(
-                children: [
-                  const SizedBox(width: 13,),
-                 CustomButtons(colors: AppColors.ambercolor, title: 'Number\n    pad',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: 'e',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '0',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.graycolor, title: '.',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                 CustomButtons(colors: AppColors.greencolor, title: '=',onpressed: () {
-                   Input = '2nd';
-                   setState(() {
-                     ();
-                   });
-                 },),
-                ],
-              ),
+            ),
+          ),
+          const SizedBox(height: 30,),
+          Row(
+            children: [
+              const SizedBox(width: 13,),
+              CustomButtons(colors: AppColors.browncolor, title: '2nd', onpressed: () => onBtnClick('2nd')),
+              CustomButtons(colors: AppColors.browncolor, title: 'rad', onpressed: () => onBtnClick('rad')),
+              CustomButtons(colors: AppColors.browncolor, title: 'sin', onpressed: () => onBtnClick('sin')),
+              CustomButtons(colors: AppColors.browncolor, title: 'cos', onpressed: () => onBtnClick('cos')),
+              CustomButtons(colors: AppColors.browncolor, title: 'tan', onpressed: () => onBtnClick('tan')),
             ],
           ),
-        ) 
         
+          Row(
+            children: [
+              const SizedBox(width: 13,),
+              CustomButtons(colors: AppColors.browncolor, title: 'x^y', onpressed: () => onBtnClick('x^y')),
+              CustomButtons(colors: AppColors.browncolor, title: 'lg', onpressed: () => onBtnClick('lg')),
+              CustomButtons(colors: AppColors.browncolor, title: 'ln', onpressed: () => onBtnClick('ln')),
+              CustomButtons(colors: AppColors.browncolor, title: '(', onpressed: () => onBtnClick('(')),
+              CustomButtons(colors: AppColors.browncolor, title: ')', onpressed: () => onBtnClick(')')),
+            ],
+          ),
+         
+          Row(
+            children: [
+              const SizedBox(width: 13,),
+              CustomButtons(colors: AppColors.browncolor, title: 'sqrt(x)', onpressed: () => onBtnClick('sqrt(x)')),
+              CustomButtons(colors: AppColors.ambercolor, title: 'AC', onpressed: () => onBtnClick('AC')),
+              CustomButtons(colors: AppColors.ambercolor, title: '<<', onpressed: () => onBtnClick('<<')),
+              CustomButtons(colors: AppColors.ambercolor, title: '%', onpressed: () => onBtnClick('%')),
+              CustomButtons(colors: AppColors.ambercolor, title: '/', onpressed: () => onBtnClick('/')),
+            ],
+          ),
+          
+          Row(
+            children: [
+              const SizedBox(width: 13,),
+              CustomButtons(colors: AppColors.browncolor, title: 'x!', onpressed: () => onBtnClick('x!')),
+              CustomButtons(colors: AppColors.graycolor, title: '7', onpressed: () => onBtnClick('7')),
+              CustomButtons(colors: AppColors.graycolor, title: '8', onpressed: () => onBtnClick('8')),
+              CustomButtons(colors: AppColors.graycolor, title: '9', onpressed: () => onBtnClick('9')),
+              CustomButtons(colors: AppColors.ambercolor, title: 'X', onpressed: () => onBtnClick('X')),
+            ],
+          ),
+          
+          Row(
+            children: [
+              const SizedBox(width: 13,),
+              CustomButtons(colors: AppColors.browncolor, title: '1/x', onpressed: () => onBtnClick('1/x')),
+              CustomButtons(colors: AppColors.graycolor, title: '4', onpressed: () => onBtnClick('4')),
+              CustomButtons(colors: AppColors.graycolor, title: '5', onpressed: () => onBtnClick('5')),
+              CustomButtons(colors: AppColors.graycolor, title: '6', onpressed: () => onBtnClick('6')),
+              CustomButtons(colors: AppColors.ambercolor, title: '-', onpressed: () => onBtnClick('-')),
+            ],
+          ),
+        
+          Row(
+            children: [
+              const SizedBox(width: 13,),
+              CustomButtons(colors: AppColors.browncolor, title: 'pi', onpressed: () => onBtnClick('pi')),
+              CustomButtons(colors: AppColors.graycolor, title: '1', onpressed: () => onBtnClick('1')),
+              CustomButtons(colors: AppColors.graycolor, title: '2', onpressed: () => onBtnClick('2')),
+              CustomButtons(colors: AppColors.graycolor, title: '3', onpressed: () => onBtnClick('3')),
+              CustomButtons(colors: AppColors.ambercolor, title: '+', onpressed: () => onBtnClick('+')),
+            ],
+          ),
+        
+          Row(
+            children: [
+              const SizedBox(width: 13,),
+              CustomButtons(colors: AppColors.ambercolor, title: 'Number\n    pad', onpressed: () => onBtnClick('Number pad')),
+              CustomButtons(colors: AppColors.graycolor, title: 'e', onpressed: () => onBtnClick('e')),
+              CustomButtons(colors: AppColors.graycolor, title: '0', onpressed: () => onBtnClick('0')),
+              CustomButtons(colors: AppColors.graycolor, title: '.', onpressed: () => onBtnClick('.')),
+              CustomButtons(colors: AppColors.greencolor, title: '=', onpressed: () => onBtnClick('=')),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
